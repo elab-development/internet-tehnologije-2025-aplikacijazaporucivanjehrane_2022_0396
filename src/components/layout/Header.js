28c04183bc6af30578d49664d0cd684ccec9374c
danilobogavac <danilobogavac22@gmail.com>
2026-01-09 16:45:18 +0100

    logout dugme sredjeno

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,6 @@
+'use client';
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
 
 function AuthLinks({status, userName}) {
   if (status === 'authenticated') {
@@ -28,6 +30,10 @@ function AuthLinks({status, userName}) {
 }
 
 export default function Header(){
+
+  const session = useSession();
+  const status = session?.status;
+
     return (
     <header className="flex items-center  justify-between">
       
@@ -42,9 +48,28 @@ export default function Header(){
        
       </nav>
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-        <Link href={'/login'}>Login</Link>
-        <Link href={'/register'} className="bg-primary rounded-full text-white px-8 py-2">Register</Link>
-      </nav>
+  {status === 'authenticated' && (
+    <button
+      onClick={() => signOut()}
+      className="bg-primary rounded-full text-white px-8 py-2"
+    >
+      Logout
+    </button>
+  )}
+
+  {status === 'unauthenticated' && (
+    <>
+      <Link href={'/login'}>Login</Link>
+      <Link
+        href={'/register'}
+        className="bg-primary rounded-full text-white px-8 py-2"
+      >
+        Register
+      </Link>
+    </>
+  )}
+</nav>
+
     </header>
     );
 }
