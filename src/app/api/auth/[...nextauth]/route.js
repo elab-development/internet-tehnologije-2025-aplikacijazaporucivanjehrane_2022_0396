286023871749052d73e5862a3ae0cf4c28687665
stojilkovicc_a <aleksandar.palja03@gmail.com>
2026-02-06 14:33:43 +0100

    porucivanje

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,16 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import clientPromise from "@/libs/mongoConnect";
+import {UserInfo} from "@/models/UserInfo";
+import bcrypt from "bcrypt"; 
 import * as mongoose from "mongoose";
-import bcrypt from "bcrypt";
 import {User} from '@/models/User';
+import NextAuth, {getServerSession} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const authOptions = {
-  
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -37,6 +40,19 @@ export const authOptions = {
     })
   ],
 };
+
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({email:userEmail});
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
 
 const handler = NextAuth(authOptions);
 
