b38b19d7ecd5dff4d363b8e833a90eef4a103caa
stojilkovicc_a <aleksandar.palja03@gmail.com>
2026-02-06 10:23:22 +0100

    meni stavke za korisnika

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,13 @@
-
+import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
 import SectionHeaders from "@/components/layout/SectionHeaders";
 
-
-
 export default function Home() {
   return (
     <>
-      
       <Hero />
       <HomeMenu />
-
       <section className="text-center my-16" id="about">
         <SectionHeaders
           subHeader={'Our story'}
@@ -37,5 +33,5 @@ export default function Home() {
         </div>
       </section>
     </>
-  );
+  )
 }
